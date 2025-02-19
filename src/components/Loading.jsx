e62514080fa458d8cf0720e8113d46c937b96304
lsmithl <smithl42@hotmail.com>
2025-02-19 12:10:09 +0000

    add individual article page

---
+++ src/components/Loading.jsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div className="loading">
+      <img src="https://media.tenor.com/1L7d8b_ZjxUAAAAj/meow-party-disco-cat.gif" />
+      <p>Loading!</p>
+    </div>
+  );
+}
